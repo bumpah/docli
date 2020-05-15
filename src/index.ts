aeac0652e2e27f34cfe2cc29069f9debbdd7a395
bumpah <jimipajala@hotmail.com>
2020-05-15 07:18:09 +0300

    add new commands

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { argv } from "process"
-import { exec } from "child_process"
+import { exec, spawn } from "child_process"
 import chalk from "chalk"
 
 const info = () => chalk`
 {yellow COMMANDS}
 {blue ps   -- show running containers}
 {blue stop -- stop all running containers}
+{blue res -- restart all running container}
+{blue b <name> -- build docker containers}
 `
 
 type Keys = keyof typeof commands
@@ -13,6 +15,7 @@ let [, , ...args] = argv
 let arg: Keys[]
 arg = <Array<Keys>>args
 
+console.log({ args })
 const run = (cmd: string) => {
   exec(cmd, (error, stdout, stderr) => {
     if (error) {
@@ -27,29 +30,61 @@ const run = (cmd: string) => {
   })
 }
 
+const runSpawn = (prog: string, args: string[]) => {
+  console.log('runSpawn', prog, { args })
+  let app = spawn(prog, args)
+
+  app.stdout.on('data', (data) => { console.log(chalk`{gray ${data}}`) })
+  app.stderr.on('data', (data) => { console.log(chalk`{red ${data}}`) })
+  app.on('exit', (_) => { console.log(chalk`{green DONE!}`) })
+}
+
 
 const commands = {
-  ps: () => {
+  ps: (args: string) => {
     run(`docker  ps --format="table {{.ID}}\t{{.Names }}"`)
   },
-  stop: () => {
+  stop: (args: string) => {
+    console.log(chalk`{yellow Stopping all running containers...}`)
     run(`docker stop $(docker ps -q)`)
   },
-  info: () => {
+  res: (args: string) => {
+    console.log(chalk`{yellow Restarting all running containers...}`)
+    run(`docker restart $(docker ps -q)`)
+  },
+  b: (args: string) => {
+    const [_, ...services] = args.split(' ')
+    console.log(chalk`Now building containers {green ${services.join(' ')}}`)
+    runSpawn(`docker-compose`, `build ${services.join(' ')}`.split(' '))
+  },
+  info: (args: string) => {
     console.log(info())
   }
 }
 
 const main = () => {
   console.log(info())
-
-  for (let cmd of arg) {
+  for (let cmd of buildCommands()) {
     try {
-      commands[cmd]()
+      if (cmd.startsWith('b ')) commands.b(cmd)
+      else commands[cmd]('')
     } catch (error) {
-      console.error(chalk`{red ${error.message}}`)
+      console.error(chalk`{red Error: tried to run "${cmd}"}`)
+      console.error(chalk`{yellow Commands: ${Object.keys(commands).join(', ')}}`)
+      console.error(chalk`{red Message: ${error.message}}`)
     }
   }
+}
+
+const buildCommands = (): Keys[] => {
+  const cmdList: Keys[] = []
+  for (let c of arg) {
+    let lastItem = cmdList.length - 1
+    if (!Object.keys(commands).includes(c))
+      cmdList[lastItem] += ` ${c}`
+    else cmdList.push(c)
+  }
+  return cmdList
 }
 
 main()
