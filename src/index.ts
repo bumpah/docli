02352f2864a188ff6777bbacb28f6fdbe4e8ce99
bumpah <jimipajala@hotmail.com>
2020-05-15 13:04:33 +0300

    add logging

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ let [, , ...args] = argv
 let arg: Keys[]
 arg = <Array<Keys>>args
 
-console.log({ args })
 const run = (cmd: string) => {
   exec(cmd, (error, stdout, stderr) => {
     if (error) {
@@ -34,11 +33,33 @@ const runSpawn = (prog: string, args: string[]) => {
   console.log('runSpawn', prog, { args })
   let app = spawn(prog, args)
 
-  app.stdout.on('data', (data) => { console.log(chalk`{gray ${data}}`) })
-  app.stderr.on('data', (data) => { console.log(chalk`{red ${data}}`) })
+  app.stdout.on('data', (data) => {
+    if (args.includes('logs')) {
+      filterOutMessages(data)
+    } else {
+      console.log(chalk`{gray ${data}}`)
+    }
+  })
+  app.stderr.on('data', (data) => {
+    if (args.includes('logs')) {
+      filterOutMessages(data)
+    } else {
+      console.log(chalk`{red ${data}}`)
+    }
+  })
   app.on('exit', (_) => { console.log(chalk`{green DONE!}`) })
 }
 
+
+const skipMessages = [
+  '[ThreadPoolExecutor-0_0] new transaction',
+  '[ThreadPoolExecutor-0_0] commit',
+  '[ThreadPoolExecutor-0_0] commit <zope.sqlalchemy.datamanager.SessionDataManager object at'
+]
+const filterOutMessages = (message: string) => {
+  const doFilter = skipMessages.find(item => message.indexOf(item) > -1)
+  if (!doFilter) console.log(chalk`${message}`)
+}
 
 const commands = {
   ps: (args: string) => {
@@ -59,9 +80,12 @@ const commands = {
     }
   },
   b: (args: string) => {
-    const [_, ...services] = args.split(' ')
-    console.log(chalk`Now building containers {green ${services.join(' ')}}`)
-    runSpawn(`docker-compose`, `build ${services.join(' ')}`.split(' '))
+    console.log(chalk`Now building containers {green ${args}`)
+    runSpawn(`docker-compose`, `build ${args}`.split(' '))
+  },
+  log: (args: string) => {
+    console.log(chalk`Viewing logs of {green ${args}}`)
+    runSpawn(`docker`, `logs --follow ${args}`.split(' '))
   },
   info: (args: string) => {
     console.log(info())
